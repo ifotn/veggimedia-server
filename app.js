9e44d032d90734d84427e264f85cb443fe54df70
ifotn <Rich.Freeman@GeorgianCollege.ca>
2024-03-22 10:48:50 -0400

    l10/2 - post

--- app.js
+++ app.js
@@ -16,6 +16,13 @@ mongoose.connect(process.env.CONNECTION_STRING, {})
 .then((res) => { console.log('Connected to MongoDB') })
 .catch((err) => { console.log(`Connection Failure: ${err}`)});
 
+// cors for angular client access
+const cors = require('cors');
+app.use(cors({
+    origin: process.env.CLIENT_URL,
+    methods: "GET,POST,PUT,DELETE,HEAD,OPTIONS"
+}));
+
 // controller + route
 const mediaController = require('./controllers/media');
 app.use('/v1/api/media', mediaController);
--- controllers/media.js
+++ controllers/media.js
@@ -2,15 +2,27 @@ const express = require('express');
 const router = express.Router();
 const Media = require('../models/media');
 
-// GET: /media 
+// GET: /media => fetch all media docs
 router.get('/', async (req, res) => {
     try {
         const media = await Media.find();
-        return res.json(media).status(200);
+        return res.status(200).json(media);
     }
     catch (err) {
-        return res.json(err).status(404);
+        return res.status(404).json(err);
     }
 });
+
+// POST: /media => save new media doc from request body
+router.post('/', async (req, res) => {
+    try {
+        const media = await Media.create(req.body);
+        return res.status(201).json(media); // 201: Resource Created
+    }
+    catch (err) {
+        return res.status(400).json(err);  // 400: Bad Request
+    }
+});
+
 
 module.exports = router;
