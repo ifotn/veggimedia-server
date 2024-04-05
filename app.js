0bff109e5a32b57e648d1134cda5e5f8fc275868
ifotn <Rich.Freeman@GeorgianCollege.ca>
2024-04-05 09:51:55 -0400

    l12/3 - cors only in dev

--- app.js
+++ app.js
@@ -9,19 +9,19 @@ app.use(bodyParser.json());
 // use .env if not in production mode
 if (process.env.NODE_ENV !== 'production') {
     require('dotenv').config();
+
+    // cors for angular client access
+    const cors = require('cors');
+    app.use(cors({
+        origin: process.env.CLIENT_URL,
+        methods: "GET,POST,PUT,DELETE,HEAD,OPTIONS"
+    }));
 }
 
 // db connect
 mongoose.connect(process.env.CONNECTION_STRING, {})
 .then((res) => { console.log('Connected to MongoDB') })
 .catch((err) => { console.log(`Connection Failure: ${err}`)});
-
-// cors for angular client access
-const cors = require('cors');
-app.use(cors({
-    origin: process.env.CLIENT_URL,
-    methods: "GET,POST,PUT,DELETE,HEAD,OPTIONS"
-}));
 
 // controller + route
 const mediaController = require('./controllers/media');
